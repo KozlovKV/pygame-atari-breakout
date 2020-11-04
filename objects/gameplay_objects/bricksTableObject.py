85b6e837c786290bfb1e7beadb5db4a17e21fcc5
Kozlov Kirill <kozlov.kirill.v.work@gmail.com>
2020-11-04 20:01:58 +0600

    Added infinite mod

--- objects/gameplay_objects/bricksTableObject.py
+++ objects/gameplay_objects/bricksTableObject.py
@@ -1,4 +1,4 @@
-from functools import reduce
+from random import randint
 
 from misc import get_random_color
 from objects.gameplay_objects.baseDrawableObject import BaseDrawableObject
@@ -6,16 +6,23 @@ from objects.gameplay_objects.baseDrawableObject import BaseDrawableObject
 
 class BricksTableObject:
     def __init__(self, game, width_count=10, height_count=5,
-                 brick_width=70, brick_height=40, padding=50, infinite=False):
+                 brick_width=70, brick_height=40, padding=50):
         self.game = game
-        self.table = [[]] * height_count
-        for i in range(height_count-1, -1, -1):
+        self.width_count = width_count
+        self.height_count = height_count
+        self.brick_width = brick_width
+        self.brick_height = brick_height
+        self.padding = padding
+        self.table = []
+        for i in range(height_count):
+            line = []
             for j in range(width_count):
                 x = padding + brick_width * j
                 y = padding + brick_height * i
-                self.table[i].append(
+                line.append(
                     BaseDrawableObject(game, x, y, brick_width, brick_height,
                                        get_random_color((0, 0, 0))))
+            self.table.append(line)
 
     def events(self, event):
         pass
@@ -31,6 +38,8 @@ class BricksTableObject:
                     line.pop(i)
                     i -= 1
                 i += 1
+        if self.game.infinite and self.get_bricks_count() <= 25:
+            self.add_new_line()
 
     def is_win(self):
         return self.get_bricks_count() == 0 and not self.game.infinite
@@ -38,11 +47,23 @@ class BricksTableObject:
     def get_bricks_count(self):
         sum = 0
         for line in self.table:
+            print(len(line))
             sum += len(line)
         return sum
 
     def add_new_line(self):
-        pass
+        for line in self.table:
+            for brick in line:
+                brick.rect.y += self.brick_height
+        new_line = []
+        for j in range(self.width_count):
+            x = self.padding + self.brick_width * j
+            y = self.padding
+            new_line.append(
+                BaseDrawableObject(self.game, x, y, self.brick_width,
+                                   self.brick_height,
+                                   get_random_color((0, 0, 0))))
+        self.table.append(new_line)
 
     def draw(self):
         for line in self.table:
